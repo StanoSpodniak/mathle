e4d5243e4fd4fff132558e352824c9e422d4ee23
StanoSpodniak <stano28@gmail.com>
2023-07-20 14:49:05 +0200

    Numbers are now clickable

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -1,40 +1,68 @@
-import {useState, useRef } from "react";
+import {useState, useEffect, useRef } from "react";
 import Game from "../Game/Game";
 import "./Main.css";
 import Draggable from 'react-draggable';
 
 const Main = () => {
-    /* Maybe get this values in useEffect - to prevent refresh with click */
-    const firstProblem = Game();
-    const secondProblem = Game();
-    const numbers = [firstProblem.firstRndNmb, firstProblem.secondRndNmb, firstProblem.thirdRndNmb];
-
+    const [firstProblem, setFirstProblem] = useState([]);
+    const [playNumbers, setPlayNumbers] = useState([]);
     const buttonRefs = useRef([]);
-    const [clickedNumber, setClickedNumber] = useState("");
 
-    const handleClick = (i) => {
-        const button = buttonRefs.current[i];
-        setClickedNumber(button.innerText);
+    const [clickedNumbers, setClickedNumbers] = useState([]);
+
+    useEffect(() => {
+        setFirstProblem(Game());
+    }, []);
+
+    useEffect(() => {
+        if (firstProblem) {
+            let numbers = [firstProblem.firstRndNmb];
+            numbers = [...numbers, firstProblem.secondRndNmb];
+            numbers = [...numbers, firstProblem.thirdRndNmb];
+            setPlayNumbers(numbers);
+        } else {
+            console.log("no data");
+        }
+    }, [firstProblem]);
+
+    const handleClick = (index) => {
+        const button = buttonRefs.current[index];
+
+        const currentPlayfield = [...clickedNumbers];
+        for(let i = 0; i < 3; i++) {
+            if(!currentPlayfield[i]) {
+                currentPlayfield[i] = button.innerText;
+                setClickedNumbers(currentPlayfield);
+                break;
+            }
+        }
     }
+
+    //add function to delete number from playfield with click, if it has a number; probably use useRef
+    //maybe block numbers in inventory if it is currently used
 
     return (
         <div className="main">
             <div className="playfield">
                 <div className="tile">
-                    {clickedNumber}
+                    {clickedNumbers[0]}
                 </div>
                 <p>{firstProblem.mathSign}</p>
-                <div className="tile"></div>
+                <div className="tile">
+                    {clickedNumbers[1]}
+                </div>
                 <p>{firstProblem.secondMathSign}</p>
-                <div className="tile"></div>
+                <div className="tile">
+                    {clickedNumbers[2]}
+                </div>
                 <p>=</p>
                 <p>{firstProblem.result}</p>
             </div>
             <button>Submit</button>
             <div className="inventory">
-                {numbers.map((number, i) => (
+                {playNumbers.map((number, i) => (
                     <button
-                        key={i} 
+                        key={i}
                         className="number-tile"
                         ref={ref => (buttonRefs.current[i] = ref)} 
                         onClick={() => handleClick(i)}
